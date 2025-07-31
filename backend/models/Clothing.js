3e16a20734851d443c20968175998bb070440afd
Busy-Platypus <busy.platypus@gmail.com>
2025-07-31 20:57:17 +0200

    Adding gender, type of cloth, quantity and id for each db item

--- backend/models/Clothing.js
+++ backend/models/Clothing.js
@@ -6,8 +6,18 @@ const clothingSchema = new mongoose.Schema({
   price: Number,
   size: String,
   material: String,
+  quantityAvailable: { type: Number, required: true }, // quantités disponibles
   imageUrl: String,
-  createdAt: { type: Date, default: Date.now }
+  sku: { type: String, default: () => uuidv4(), unique: true }, // identifiant unique
+  createdAt: { type: Date, default: Date.now },
+  category: {
+    type: String,
+    required: true,
+    enum: ['pantalon', 'robe', 'chemise', 'tshirt', 'jupe', 'manteau']},
+  gender: {
+    type: String,
+    required: true,
+    enum: ['homme', 'femme', 'unisexe']}
 });
 
 module.exports = mongoose.model('Clothing', clothingSchema);
--- backend/routes/clothing.js
+++ backend/routes/clothing.js
@@ -28,14 +28,26 @@ const parser = multer({ storage: storage });
 // POST /api/clothing (protégé, upload image)
 router.post('/', auth, parser.single('image'), async (req, res) => {
   try {
-    const { name, description, price, size, material } = req.body;
+      const { name, description, price, size, material, quantity, category, gender } = req.body;
     let imageUrl = null;
 
     if (req.file) {
       imageUrl = req.file.path;  // URL de l’image Cloudinary
     }
 
-    const newClothing = new Clothing({ name, description, price, size, material, imageUrl });
+    const newClothing = new Clothing({
+      name,
+      description,
+      price: parseFloat(price),
+      size,
+      material,
+      quantityAvailable: parseInt(quantity),
+      sku: uuidv4(),
+      category,
+      gender,
+      imageUrl
+    });
+
     await newClothing.save();
 
     res.status(201).json({ message: 'Vêtement ajouté', clothing: newClothing });
@@ -45,6 +57,7 @@ router.post('/', auth, parser.single('image'), async (req, res) => {
   }
 });
 
+const { v4: uuidv4 } = require('uuid'); //
 // GET /api/clothing - récupérer tous les vêtements
 router.get('/', async (req, res) => {
   try {
@@ -54,7 +67,5 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: 'Erreur serveur' });
   }
 });
-
-
 
 module.exports = router;
